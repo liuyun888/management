9827ed691279f6c7e0c52b7676b005fecdf85551
liuyun888 <632233796@qq.com>
2018-11-01 18:03:45 +0800

    userController 添加 获取用户角色、权限信息。  jsonObject 的使用。。。。

--- src/main/java/com/permission/management/core/controller/UserInfoController.java
+++ src/main/java/com/permission/management/core/controller/UserInfoController.java
@@ -37,8 +37,10 @@ public class UserInfoController {
         //名称（昵称或者真实姓名）
         result.put("name", userInfo.get("name"));
 
-        /**把JSONArray(roleJsonArray)转换为List对象*/
+        /**把JSONArray 转换为List对象,然后操作List*/
+        //将userInfo中的角色列表信息转换为JSONArray
         JSONArray roleListArray = (JSONArray) userInfo.get("roleList");
+        //权限在角色数据中
         if(roleListArray.size() > 0) {
             //拥有的角色
             List rolelist = getRoles(roleListArray);
@@ -47,42 +49,46 @@ public class UserInfoController {
             List permissionlist = getPermission(roleListArray);
             result.put("permissionlist", permissionlist);
         }
-
         return result;
     }
     //获取拥有的角色
     private List getRoles(JSONArray roleListArray) {
-        List<SysRole> roleJonList = new ArrayList<>();
+        //角色List
         List roleList = new ArrayList();
+        //遍历userInfo的角色List
         for (int i = 0; i < roleListArray.size(); i++) {
+            //转化为JSONObject
             JSONObject roleJson = (JSONObject) roleListArray.get(i);
+            //转为SysRole实体
             SysRole roleJsonDate = (SysRole) JSONObject.toBean(roleJson, SysRole.class);
-            roleJonList.add(roleJsonDate);
-            roleList.add(roleJonList.get(i).getRole());
+            //获取实体中角色数据
+            roleList.add(roleJsonDate.getRole());
         }
         return roleList;
     }
-    //获取拥有的权限
+    //从角色列表数据中获取拥有的权限
     private List getPermission(JSONArray roleListArray) {
-        List<SysRole> roleJonList = new ArrayList<>();
-        List<SysPermission> permissionJonList = new ArrayList<>();
+        //权限List
         List permissions = new ArrayList();
+        //遍历userInfo的角色List
         for (int i = 0; i < roleListArray.size(); i++) {
+            //转化为JSONObject
             JSONObject roleJson = (JSONObject) roleListArray.get(i);
+            //转为SysRole实体
             SysRole roleJsonDate = (SysRole) JSONObject.toBean(roleJson, SysRole.class);
-            roleJonList.add(roleJsonDate);
-
-            List permissionList = roleJonList.get(i).getPermissions();
+            //获取实体中当前角色的权限数据
+            List permissionList = roleJsonDate.getPermissions();
             if(permissionList.size() > 0)
             {
+                //遍历当前角色的权限数据
                 for (int j = 0; j < permissionList.size() ; j++) {
+                    //转化为JSONObject
                     JSONObject  permissionJson =JSONObject.fromObject(permissionList.get(j));
+                    //转为SysPermission实体
                     SysPermission permissionJsonDate = (SysPermission) JSONObject.toBean(permissionJson, SysPermission.class);
-                    permissionJonList.add(permissionJsonDate);
-
-                    permissions.add(permissionJonList.get(j).getPermission());
+                    //获取实体中权限数据
+                    permissions.add(permissionJsonDate.getPermission());
                 }
-                permissionList.clear();
             }
         }
         return permissions;
